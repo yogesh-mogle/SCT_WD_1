696a18465904499e8b20ecd03d80755b40ea76f7
yogesh-mogle <mogaley9@gmail.com>
2026-01-03 15:23:49 +0530

    Add files via upload

---
+++ InteractiveNavigation.js
@@ -0,0 +1,19 @@
+const navbar = document.getElementById("navbar");
+const links = document.querySelectorAll(".nav-links a");
+
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 80) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
+
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        links.forEach(l => l.classList.remove("active"));
+        link.classList.add("active");
+    });
+});
